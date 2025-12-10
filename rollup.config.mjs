6490f2bcf648481ae3260fcffd3477a06268ae74
elisandrof07 <f07nobru@gmail.com>
2025-12-10 19:20:44 +0100

    teste

---
+++ rollup.config.mjs
@@ -0,0 +1,5 @@
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+// ...existing config code...
+// Copiado de rollup.config.js, ajuste conforme necessário.
